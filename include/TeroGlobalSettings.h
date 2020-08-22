cf76afb6b1fae5cc0d1bbf0838af1f28355454fd
Emiliano Dalla Verde Marcozzi <edvm@fedoraproject.org>
2020-08-22 16:47:18 -0300

    Laburando en funcion para conectarse al wifi. Agrego headers con settings globales

---
+++ include/TeroGlobalSettings.h
@@ -0,0 +1,6 @@
+#define WIFI_SSID               "Campo"                     // SSID de la lanza maestro
+#define WIFI_PASSWORD           "1234"                      // Password para conectarse a la lanza maestro
+#define WIFI_CONNECT_TIMEOUT    5000                        // Timeout max al momento de tratar de conectarse a la lanza maestro
+#define WIFI_RETRY_TIMEOUT      1000                        // 1 segundo 
+#define CHUCARO_IP              (13, 59, 165, 236)          // IP del Servidor UDP
+#define CHUCARO_PORT            1234                        // Puerto del servidor UDP
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "ESPAsyncUDP.h"
+#include "TeroGlobalSettings.h"
+
+
+void connectToWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+  while( (WiFi.waitForConnectResult(WIFI_CONNECT_TIMEOUT)) != WL_CONNECTED ) {
+    Serial.println("WiFi Failed");
+    yield();
+  }
+
+}
 
 void setup() {
   // put your setup code here, to run once:
